4e4bc09437579d57960e5b197d3396fe1c6cb2f5
John Zidrex <john.andag@monstar-lab.com>
2023-06-08 18:12:09 +0800

    initial commit

---
+++ src/test/java/com/jandag/batchprocessingdemo/BatchProcessingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jandag.batchprocessingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
